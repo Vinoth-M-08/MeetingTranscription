496c186ae5865fceff8921831bd31b49678c8bd2
Vinoth_M <117703625+Vinoth-M-08@users.noreply.github.com>
2025-06-07 21:55:45 +0530

    Initial Commit For FrontEnd

---
+++ Client/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './src/Common.env' });
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
